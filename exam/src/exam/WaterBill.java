dd1976ce91a308d75052610e7e0cdd35a0e31dfa
admin <admin@Bcalab4-m21>
2022-05-21 13:11:16 +0530

    commit waterbill

---
+++ exam/src/exam/WaterBill.java
@@ -0,0 +1,16 @@
+package exam;
+
+public class WaterBill {
+
+	String acctnum;
+	int unitsbilled;
+	String name;
+
+
+WaterBill(String acctnum, int numunits){
+
+		this.acctnum=acctnum;
+		this.unitsbilled=numunits;
+
+	
+}
